4c24dcb2c18e1f9e74f38894191391efe161845e
morozoffv <22277010+morozoffv@users.noreply.github.com>
2017-12-04 11:29:20 +0400

    add CardView to ListMainFragment

--- app/src/main/java/com/example/vlad/commitsupervisor/ListMainFragment.kt
+++ app/src/main/java/com/example/vlad/commitsupervisor/ListMainFragment.kt
@@ -4,6 +4,7 @@ package com.example.vlad.commitsupervisor
 import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.CardView
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
